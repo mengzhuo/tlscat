945fbcc3e8eedc71989097d574b2623e5b056934
Meng Zhuo <mengzhuo1203@gmail.com>
2015-05-06 15:13:06 +0800

    add starttls

--- tlscat.go
+++ tlscat.go
@@ -19,15 +19,56 @@ var (
 	certificationFile = flag.String("c", "", "Certification file")
 	privateKeyFile    = flag.String("k", "", "Private Key file")
 	verbose           = flag.Bool("v", false, "Verbose Output")
+	starttls          = flag.Bool("s", false, "SMTP STARTTLS support")
 )
 
+func verbosePrintf(str string, args ...interface{}) {
+	if *verbose {
+		fmt.Printf(str, args...)
+	}
+}
+
+func errorCheckf(err error, str string, args ...interface{}) {
+
+	if err != nil {
+		fmt.Printf(str, args...)
+		fmt.Printf("Due to %s\n", err)
+		if *verbose {
+			panic(err)
+		}
+		os.Exit(1)
+	}
+}
+
 func main() {
+
 	flag.Parse()
 	if *listenPort < 65536 && 0 < *listenPort {
 		ServerMode()
 	} else {
 		ClientMode()
 	}
+}
+
+func recv(conn net.Conn) {
+
+	b := make([]byte, 2048)
+	conn.Read(b)
+	verbosePrintf("Starttls recv:%s", b)
+}
+
+func StartTLS(conn net.Conn) bool {
+
+	recv(conn)
+
+	conn.Write([]byte("EHLO example.com\r\n"))
+	recv(conn)
+
+	conn.Write([]byte("STARTTLS\r\n"))
+	recv(conn)
+	recv(conn)
+
+	return true
 }
 
 func ClientMode() {
@@ -39,15 +80,24 @@ func ClientMode() {
 	}
 	addr := args[0]
 	port := args[1]
-	if *verbose {
-		fmt.Printf("Connecting %s:%s\n", addr, port)
+	verbosePrintf("Connecting %s:%s\n", addr, port)
+
+	var conn net.Conn
+	var err error
+	endpoint := fmt.Sprintf("%s:%s", addr, port)
+	tls_config := &tls.Config{ServerName: addr}
+
+	if *starttls {
+		conn, err = net.Dial("tcp", endpoint)
+		errorCheckf(err, "Connect failed %s", endpoint)
+		if StartTLS(conn) {
+			conn = tls.Client(conn, tls_config)
+		}
+	} else {
+		conn, err = tls.Dial("tcp", endpoint, tls_config)
+		errorCheckf(err, "Can not connect to %s:%s", addr, port)
 	}
 
-	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%s", addr, port), &tls.Config{})
-	if err != nil {
-		fmt.Printf("Can not connect to %s:%s due to %s", addr, port, err)
-		panic(err)
-	}
 	go func() {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
@@ -64,10 +114,12 @@ func ClientMode() {
 	}()
 	for {
 		b := make([]byte, 2048)
-		n, _ := conn.Read(b)
+		n, err := conn.Read(b)
+		errorCheckf(err, "Error")
 		if n != 0 {
 			fmt.Print(string(b))
 		} else {
+			verbosePrintf("Disconnected")
 			break
 		}
 	}
@@ -82,10 +134,7 @@ func ServerMode() {
 
 	if *certificationFile != "" && *privateKeyFile != "" {
 		certPair, err = tls.LoadX509KeyPair(*certificationFile, *privateKeyFile)
-		if err != nil {
-			fmt.Printf("Parsing error:%s", err)
-			return
-		}
+		errorCheckf(err, "Parsing Error")
 	} else {
 		// Load a made cert
 	}
